94005154ce36251dee58a321ba3b6ad8065a8cb8
Shashank <34152717+iThink32@users.noreply.github.com>
2018-03-13 18:49:51 +0530

    Two way communication using DarwinNotificationCenter

---
+++ DistributedNotificationCenter.swift
@@ -0,0 +1,39 @@
+//
+//  DistributedNotificationsObserver.swift
+//  CheckoutOtpelf
+//
+//  Created by N.A Shashank on 08/03/18.
+//
+
+import UIKit
+
+public final class DistributedNotificationCenter{
+    
+    public class func registerObservers(arrNotificationName:[String],observer:AnyObject) {
+        let observer = UnsafeRawPointer(Unmanaged.passUnretained(observer).toOpaque())
+        arrNotificationName.forEach { (name) in
+            CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), observer, DistributedNotificationCenter.callBack(),name as CFString,nil, CFNotificationSuspensionBehavior.deliverImmediately)
+        }
+    }
+    
+    private class func callBack() -> CFNotificationCallback {
+        return { (center,observer,name,object,userInfo) in
+            guard let unwrappedName = name else{
+                return
+            }
+            NotificationCenter.default.post(name: NSNotification.Name(String(describing: unwrappedName.rawValue)), object: nil)
+        }
+    }
+    
+    
+    class func removeObservers(observer:UIViewController) {
+        let observer = UnsafeRawPointer(Unmanaged.passUnretained(observer).toOpaque())
+        CFNotificationCenterRemoveEveryObserver(CFNotificationCenterGetDarwinNotifyCenter(), observer)
+    }
+    
+    class func postNotification(name:String) {
+        CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFNotificationName.init(rawValue: name as CFString), nil, nil, true)
+    }
+    
+    
+}
